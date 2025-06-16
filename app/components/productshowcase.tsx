3291a274fa6da2be899168aacfda4b80c2931f1c
joycetagbovi <joyceabena19@gmail.com>
2025-06-16 20:04:46 +0000

    update image path

--- app/components/productshowcase.tsx
+++ app/components/productshowcase.tsx
@@ -7,9 +7,9 @@ const ProductShowcase = () => {
         {/* ZX9 Speaker */}
         <div className="zx9-speaker px-6 lg:pt-24 py-16  lg:px-24 rounded-[8px] flex flex-col lg:flex-row items-center justify-center text-white overflow-hidden lg:h-[560px] h-full">
           <div className="lg:w-1/2  lg:mb-0  mb-6 ">
-            <img src="/public/assets/home/desktop/image-speaker-zx9.png" alt="zx9 speaker" className="w-[410px] h-[493px]  max-w-full hidden lg:block" />
-            <img src="/public/assets/home/mobile/image-speaker-zx9.png" alt="zx9 speaker" className="  max-w-full md:hidden block" />
-            <img src="/public/assets/home/tablet/image-speaker-zx9.png" alt="zx9 speaker" className="  max-w-full hidden lg:hidden md:block " />
+            <img src="/assets/home/desktop/image-speaker-zx9.png" alt="zx9 speaker" className="w-[410px] h-[493px]  max-w-full hidden lg:block" />
+            <img src="/assets/home/mobile/image-speaker-zx9.png" alt="zx9 speaker" className="  max-w-full md:hidden block" />
+            <img src="/assets/home/tablet/image-speaker-zx9.png" alt="zx9 speaker" className="  max-w-full hidden lg:hidden md:block " />
           </div>
           <div className="lg:w-1/2 lg:pl-12 text-center lg:text-left">
             <h2 className="text-[36px] md:text-[56px] font-bold mb-6 tracking-[2px] uppercase leading-none">ZX9<br />SPEAKER</h2>
@@ -35,7 +35,7 @@ const ProductShowcase = () => {
         {/* YX1 Earphones */}
         <div className="grid md:grid-cols-2 gap-8">
           <div className="">
-            <img src="/public/assets/home/desktop/image-earphones-yx1.jpg" alt="earphone"  className="rounded-[8px] w-full"/>
+            <img src="/assets/home/desktop/image-earphones-yx1.jpg" alt="earphone"  className="rounded-[8px] w-full"/>
           </div>
           <div className="bg-light-gray p-10  lg:p-24 rounded-[8px] flex flex-col justify-center items-start">
             <h2 className="text-[28px] text-black font-bold mb-6 tracking-[2px]">YX1 EARPHONES</h2>
